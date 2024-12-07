47f5c7003546b2f2a5d2b412cf9f132b6fe2462e
TMedalith <u20211f955@upc.edu.pe>
2024-12-07 11:50:25 -0500

    feat: Implement user authentication with sign-up and sign-in endpoints

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
+from fastapi import FastAPI, HTTPException
+from app.config.db import create_all_tables
+from app.auth.routes import auth  # Importamos las rutas de auth
+from app.config.routes import prefix  # Importamos el prefijo desde la configuración
 
-from typing import Union
+# Crea las tablas en la base de datos si no existen
+try:
+    create_all_tables()
+except Exception as e:
+    raise HTTPException(status_code=500, detail=f"Error al crear tablas: {e}")
 
-from fastapi import FastAPI
-
-
-
-
+# Inicializa la aplicación FastAPI
 app = FastAPI()
 
+# Incluye las rutas de auth en la aplicación con el prefijo adecuado
+app.include_router(auth, prefix=prefix, tags=["Auth"])
 
-
-
-
-@app.get("/items/{item_id}")
-
-def read_item(item_id: int, q: Union[str, None] = None):
-
-    return {"item_id": item_id, "q": q}
-
+#
