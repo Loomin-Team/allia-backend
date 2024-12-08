19b23c057f525c0116a5d72c59ac43cb7f77ea2d
Cristina <avicon95@protonmail.com>
2024-12-08 17:23:03 -0500

    feat: add webscrapping serapi

--- app/news/services/bing_scraper.py
+++ app/news/services/bing_scraper.py
@@ -1,36 +1,36 @@
-from serpapi import GoogleSearch
+import os
 from app.news.services.serpapi_web_scraper import SerpApiWebScraper
+from serpapi import GoogleSearch
 
-class GoogleNewsWebScraper(SerpApiWebScraper):
+
+class BingNewsWebScraper(SerpApiWebScraper):
     """
-    A class to fetch, process, and extract detailed news content using the SerpAPI Google News Engine.
+    A class to fetch and process Bing News content using the SerpAPI Bing News Engine.
     """
 
-    def get_news(self, query, language="en", max_results=3):
+    def get_news(self, query, language="en", max_results=10):
         """
-        Fetch news articles based on a query.
-        :param query: Search query string.
+        Fetch news articles based on the query.
+        :param query: Search term for news articles.
         :param language: Language for the results.
         :param max_results: Maximum number of results to fetch.
         :return: Concatenated string of headers and bodies.
         """
-
         params = {
-            "engine": "google",
+            "engine": "bing_news",
             "q": query,
-            "tbm": "nws",
-            "hl": language,
+            "cc": language,  # Language or region code
             "api_key": self.api_key,
         }
 
         search = GoogleSearch(params)
         results = search.get_dict()
 
-        if "news_results" not in results:
-            print(f"No news results found for query in Google: {query}")
+        if "organic_results" not in results:
+            print(f"No organic results found for query: {query}")
             return ""
 
-        articles = results["news_results"][:max_results]
+        articles = results["organic_results"][:max_results]
 
         # Fetch and concatenate headers and bodies
         concatenated_content = ""
--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
 
+
 http://127.0.0.1:8000/docs
 
 
 
+Run app:
 python -m uvicorn app.main:app --reload
+
+
+docker compose for mysql:
+ docker compose -f ./docker-compose.yml up 
