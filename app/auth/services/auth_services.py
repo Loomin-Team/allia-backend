3b9098b8045bc50a1f70490824a0af385f8cecb3
alanegd <alangalavisd@gmail.com>
2024-12-08 22:40:29 -0500

    fix: Issue with full name sentence case

--- app/auth/services/auth_services.py
+++ app/auth/services/auth_services.py
@@ -13,24 +13,27 @@ ALGORITHM = os.getenv('ALGORITHM')
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 class AuthServices:
     @staticmethod
     def authenticate_user(email: str, password: str, db: Session):
         user = db.query(User).filter(User.email == email).first()
         if not user:
-            return None  
+            return None
         if not bcrypt_context.verify(password, user.password):
-            return None 
+            return None
         return user
 
     @staticmethod
     async def sign_up(create_user_request: UserSchemaPost, db: Session):
-        existing_user = db.query(User).filter(User.email == create_user_request.email).first()
+        existing_user = db.query(User).filter(
+            User.email == create_user_request.email).first()
         if existing_user:
             raise HTTPException(status_code=400, detail="Email ya registrado.")
-        
-        hashed_password = bcrypt_context.hash(create_user_request.password.encode("utf-8"))
-        
+
+        hashed_password = bcrypt_context.hash(
+            create_user_request.password.encode("utf-8"))
+
         new_user = User(
             fullname=create_user_request.fullname,
             email=create_user_request.email,
@@ -42,30 +45,34 @@ class AuthServices:
         db.commit()
         db.refresh(new_user)
 
-        
-        token = AuthServices.create_access_token(new_user.email, new_user.id, new_user.registered, timedelta(hours=1))
-        return Token(access_token=token, token_type="bearer", user={"id": new_user.id, "fullName": new_user.fullname, "email": new_user.email})
+        token = AuthServices.create_access_token(
+            new_user.email, new_user.id, new_user.registered, timedelta(hours=1))
+        return Token(access_token=token, token_type="bearer", user={"id": new_user.id, "fullname": new_user.fullname, "email": new_user.email})
 
     @staticmethod
     async def sign_in(create_user_request: CreateUserRequest, db: Session):
-        user = AuthServices.authenticate_user(create_user_request.email, create_user_request.password, db)
+        user = AuthServices.authenticate_user(
+            create_user_request.email, create_user_request.password, db)
         if not user:
-            raise HTTPException(status_code=401, detail="Email o contraseña inválidos.")
-        token = AuthServices.create_access_token(user.email, user.id, user.registered, timedelta(hours=1))
-        
+            raise HTTPException(
+                status_code=401, detail="Email o contraseña inválidos.")
+        token = AuthServices.create_access_token(
+            user.email, user.id, user.registered, timedelta(hours=1))
+
         return {
             "access_token": token,
             "token_type": "bearer",
             "user": {
                 "id": user.id,
-                "fullName": user.fullname,
+                "fullname": user.fullname,
                 "email": user.email,
             }
         }
 
     @staticmethod
     def create_access_token(email: str, user_id: int, registered: bool, expires_delta: timedelta):
-        to_encode = {"sub": email, "user_id": user_id, "registered": registered}
+        to_encode = {"sub": email, "user_id": user_id,
+                     "registered": registered}
         expire = datetime.utcnow() + expires_delta
         to_encode.update({"exp": expire})
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
